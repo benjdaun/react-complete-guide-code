3b7a6107fe7c3dad004df78765e6eea862910bab
benjdaun <67205717+benjdaun@users.noreply.github.com>
2022-01-26 22:34:40 -0500

    Title changes to updated.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-function ExpenseItem(props) {
- const [title, setTitle] = useState(props.title);
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
     setTitle('Updated!!!');
     console.log(title);
@@ -12,7 +12,7 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
