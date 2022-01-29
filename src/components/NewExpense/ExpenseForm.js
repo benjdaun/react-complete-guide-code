2d30bd00b34512963a9b531b47f98af2bff1435e
benjdaun <67205717+benjdaun@users.noreply.github.com>
2022-01-29 17:00:20 -0500

    Change field layout on form.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -28,38 +28,41 @@ const ExpenseForm = (props) => {
   };
   return (
     <form onSubmit={submitHandler}>
-      <div className="new-expense__controls"></div>
-      <div className="new-expense__control">
+    <div className='new-expense__controls'>
+      <div className='new-expense__control'>
         <label>Title</label>
-        <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+        <input
+          type='text'
+          value={enteredTitle}
+          onChange={titleChangeHandler}
+        />
       </div>
-      <div className="new-expense__control">
+      <div className='new-expense__control'>
         <label>Amount</label>
         <input
-          type="number"
+          type='number'
+          min='0.01'
+          step='0.01'
           value={enteredAmount}
           onChange={amountChangeHandler}
-          min="0.01"
-          step="0.01"
         />
       </div>
-      <div className="new-expense__control">
+      <div className='new-expense__control'>
         <label>Date</label>
         <input
-          type="date"
+          type='date'
+          min='2019-01-01'
+          max='2022-12-31'
           value={enteredDate}
           onChange={dateChangeHandler}
-          min="2019-01-01"
-          step="2022-12-31"
         />
       </div>
-      <div className="new-expense__actions">
-        <button type="button" onClick={props.onCancel}>
-          Cancel
-        </button>
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+    </div>
+    <div className='new-expense__actions'>
+      <button type="button" onClick={props.onCancel}>Cancel</button>
+      <button type='submit'>Add Expense</button>
+    </div>
+  </form>
   );
 };
 
