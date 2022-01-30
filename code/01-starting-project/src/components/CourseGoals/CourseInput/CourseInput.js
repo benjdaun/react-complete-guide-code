705bcf1bfe16e4e154bc2d3e86b6df7cdc356579
benjdaun <67205717+benjdaun@users.noreply.github.com>
2022-01-30 12:24:44 -0500

    Fix Styled Components.

--- code/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ code/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,58 +1,51 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
-  &label {
+  & label {
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' : 'black')};
   }
 
-  &input {
+  & input {
     display: block;
     width: 100%;
-    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')} ;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
   }
 
-  &input:focus {
+  & input:focus {
     outline: none;
     background: #fad0ec;
     border-color: #8b005d;
   }
-
-  &.invalid input {
-    border-color: red;
-    background-color: rgb(255, 193, 193);
-  }
-
-  &.invalid label {
-    color: red;
-  }
 `;
 
-const CourseInput = (props) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isValid, setisValid] = useState(true);
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = (event) => {
+  const goalInputChangeHandler = event => {
     if (event.target.value.trim().length > 0) {
-      setisValid(true);
+      setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = (event) => {
+  const formSubmitHandler = event => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
-      setisValid(false);
+      setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
