bf7695d3d7d873dfcae9b02e97048d5a3c742298
benjdaun <67205717+benjdaun@users.noreply.github.com>
2022-01-30 21:14:54 -0500

    Button Styled okay.

--- code/01-starting-project/src/Components/Button.js
+++ code/01-starting-project/src/Components/Button.js
@@ -3,7 +3,7 @@ import styles from './Button.module.css'
 const Button = props => {
 
     return(
-        <button>Add User</button>
+        <button className={styles.button} >Add User</button>
     )
 };
 
